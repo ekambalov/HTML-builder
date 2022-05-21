4f47e9752f47d7d297d2dc7ad5af0c9889e5fd66
ekambalov <emakcomp@gmail.com>
2022-05-21 15:31:16 +0200

    feat: create 4 task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,4 +1,3 @@
-
 const { readdir, stat } = require('fs/promises');
 const path = require('path');
 const { stdout } = process;
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const { readdir, mkdir, copyFile } = require('fs/promises');
+const path = require('path');
+const { stdout } = process;
 
+const folderPath = path.join(__dirname, 'files');
+const folderPathCopy = path.join(__dirname, 'files-copy');
+
+(async (dirPath, dirPathCopy) => {
+  let filePath =[];
+  let filePathCopy =[];
+  const files = await readdir(dirPath, { withFileTypes: true });
+  for(const file of files){
+    if(file.isFile()){
+      filePath.push(path.join(dirPath, file.name));
+      filePathCopy.push(path.join(dirPathCopy, file.name));
+    }
+  }
+  const mkDir = await mkdir(dirPathCopy, {recursive: true});
+  console.log(dirPathCopy);
+  for(let i = 0; i < filePath.length; i++) {
+    const copy = await copyFile(filePath[i], filePathCopy[i]);
+  }
+})(folderPath, folderPathCopy);
