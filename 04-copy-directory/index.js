8b8737be9649f96ac931f9805a553e39aedb2b31
ekambalov <emakcomp@gmail.com>
2022-05-22 14:22:58 +0200

    fix: fix 4 task due eslint

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,6 +1,5 @@
 const { readdir, mkdir, copyFile } = require('fs/promises');
 const path = require('path');
-const { stdout } = process;
 
 const folderPath = path.join(__dirname, 'files');
 const folderPathCopy = path.join(__dirname, 'files-copy');
@@ -15,9 +14,8 @@ const folderPathCopy = path.join(__dirname, 'files-copy');
       filePathCopy.push(path.join(dirPathCopy, file.name));
     }
   }
-  const mkDir = await mkdir(dirPathCopy, {recursive: true});
-  console.log(dirPathCopy);
+  await mkdir(dirPathCopy, {recursive: true});
   for(let i = 0; i < filePath.length; i++) {
-    const copy = await copyFile(filePath[i], filePathCopy[i]);
+    await copyFile(filePath[i], filePathCopy[i]);
   }
 })(folderPath, folderPathCopy);
