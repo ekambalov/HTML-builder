e2ca90e07389c46e1d6b3d996580fb8f14e24d96
ekambalov <emakcomp@gmail.com>
2022-05-22 17:01:45 +0200

    feat: create 05 task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,34 @@
+const { readdir, mkdir, copyFile } = require('fs/promises');
+const path = require('path');
+const fs = require('fs');
+
+const folderPath = path.join(__dirname, 'styles');
+const filePathBundle = path.join(__dirname, 'project-dist', 'bundle.css');
+let stylesData = [];
+
+const writeStream = fs.createWriteStream(
+  filePathBundle,
+  'utf-8',
+);
+
+async function copyData (filePath) {
+  const readStream = fs.createReadStream(filePath);
+  for await (const chunk of readStream) {
+    stylesData.push(chunk);
+  }
+}
+
+
+(async (dirPath) => {
+  const files = await readdir(dirPath, { withFileTypes: true });
+  for(const file of files){
+    if(file.isFile()){ 
+      const fileParse = path.parse(path.join(dirPath, file.name));
+      if(fileParse.ext === '.css'){
+        await copyData(path.join(dirPath, file.name));
+      }  
+    }
+  }
+  writeStream.write(stylesData.join('\n').toString());
+
+})(folderPath);
